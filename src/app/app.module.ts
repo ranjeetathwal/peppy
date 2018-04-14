23f51a690d21961b519c50a862b77228df22f3ac
Ranjeet Athwal <ranjeet.athwal@gmail.com>
2018-04-14 12:36:10 +0100

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
+import { StoriesComponent } from './stories/stories.component';
+import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
+import { AppRouterModule } from './app-router.module';
+import { CharactersService } from './characters-service.service';
+import { Http, HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    StoriesComponent,
+    AboutComponent,
+    CharactersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CharactersService, Http],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
